af540eb727efe4cc4b8a1d0b337f7bb7175957de
pochi <pochi.black@gmail.com>
2011-04-01 17:54:32 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thankyouworld::Application.config.secret_token = 'f3e668780c1a93e50b983e4767a736ea0fbe5c94af2c7f0507714b5c31567250df332218e9271d013cd2478880c11e742a72dfc3f638284e6c310d687b7c1ef1'
